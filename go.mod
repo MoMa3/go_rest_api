c0355c360431b35645d3f1679af36fb2aaaf37c2
mo <mohanad.mazin188@hotmail.com>
2024-04-23 18:50:55 -0400

    dummy route

---
+++ go.mod
@@ -0,0 +1,3 @@
+module exmaple/go_rest_api
+
+go 1.18
---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"math/rand"
+	"net/http"
+	"fmt"
+  )
+
+
+const creditScoreMin = 500
+const creditScoreMax = 900
+
+type credit_rating struct {
+  CreditRating int `json:"credit_rating"`
+}
+
+func getCreditScore(w http.ResponseWriter, r *http.Request) {
+	var creditRating = credit_rating{
+		CreditRating: (rand.Intn(creditScoreMax-creditScoreMin) + creditScoreMin),
+	}
+	fmt.Println(creditRating)
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(creditRating)
+  }
+
+  func handleRequests() {
+	http.Handle("/creditscore", http.HandlerFunc(getCreditScore))
+	log.Fatal(http.ListenAndServe(":8080", nil))
+  }
+  
+  func main() {
+	handleRequests()
+  }
